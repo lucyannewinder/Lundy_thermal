fde92142f61a516b8a0e2d64dab91548a8036114
lucyannewinder <47522409+lucyannewinder@users.noreply.github.com>
2024-02-27 14:49:13 +0000

    Update TB.heritability.new.R

--- TB.heritability.new.R
+++ TB.heritability.new.R
@@ -63,6 +63,29 @@ Age12.tb<-droplevels(Age12.tb)
 
 length(Age12.tb$max.temp)
 
+#add brood size
+
+Age2.tb <- Age2.tb %>%
+  group_by(Brood.natal) %>%
+  mutate(Natal_brood_size = n())
+Age5.tb <- Age5.tb %>%
+  group_by(Brood.natal) %>%
+  mutate(Natal_brood_size = n())
+Age5.tb <- Age5.tb %>%
+  group_by(Brood.rearing) %>%
+  mutate(Rearing_brood_size = n())
+Age10.tb <- Age10.tb %>%
+  group_by(Brood.natal) %>%
+  mutate(Natal_brood_size = n())
+Age10.tb <- Age10.tb %>%
+  group_by(Brood.rearing) %>%
+  mutate(Rearing_brood_size = n())
+Age12.tb <- Age12.tb %>%
+  group_by(Brood.natal) %>%
+  mutate(Natal_brood_size = n())
+Age12.tb <- Age12.tb %>%
+  group_by(Brood.rearing) %>%
+  mutate(Rearing_brood_size = n())
 
 prior_Tbher<-list(
   R=list(V=1, nu=0.002), 
@@ -75,10 +98,15 @@ prior_Tbher<-list(
 a=20
 
 
+unique_to_vector2 <- setdiff(Age2.tb$BirdID, Tb.ped.new$BirdID)
+Age2.tb <- subset(Age2.tb, BirdID != "10179")
+
+
 Age2.tb$femID<-as.factor(Age2.tb$femID)
 
 modM1_Tbher_2 <- MCMCglmm(max.temp~Air.temp+
-                            as.factor(Year),
+                            as.factor(Year)+
+                            Natal_brood_size,
                      random=~BirdID+
                        Brood.natal,
                      data=Age2.tb,
@@ -87,7 +115,7 @@ modM1_Tbher_2 <- MCMCglmm(max.temp~Air.temp+
 summary(modM1_Tbher_2)
 plot(modM1_Tbher_2)
 
-posther.2<-modM1_Tbher_2$VCV[,"units"]/(modM1_Tbher_2$VCV[,"BirdID"]+modM1_Tbher_2$VCV[,"Brood.natal"]+modM1_Tbher_2$VCV[,"units"])
+posther.2<-modM1_Tbher_2$VCV[,"BirdID"]/(modM1_Tbher_2$VCV[,"BirdID"]+modM1_Tbher_2$VCV[,"Brood.natal"]+modM1_Tbher_2$VCV[,"units"])
 HPDinterval(posther.2,0.95)
 posterior.mode(posther.2)
 
@@ -95,9 +123,25 @@ posterior.mode(posther.2)
 
 plot(posther.2)
 
+Tb.ped<-read.csv("pedto19_new.csv")
+colnames(Tb.ped)[1] <- "BirdID"
+Tb.ped<- Tb.ped %>%
+  dplyr::select(BirdID,
+                Dam,
+                Sire)
+missing.birds<-unique(Age5.tb$BirdID[which(!Age5.tb$BirdID%in%Tb.ped[,1])])
+Tb.ped<-insertPed(Tb.ped,missing.birds)
+Tb.ped.new<- prunePed(orderPed(Tb.ped[,1:3]),unique(Age5.tb$BirdID), make.base = TRUE)
+TB.Ainv<-inverseA(Tb.ped.new)$Ainv
+
+unique_to_vector2 <- setdiff(Age5.tb$BirdID, Tb.ped.new$BirdID)
+values_to_remove<-c("10179" ,"10821", "10827", "10829" ,"10911")
+Age5.tb <- subset(Age5.tb, !BirdID %in% values_to_remove)
+Age5.tb<-as.data.frame(Age5.tb)
 
 modM1_Tbher_5 <- MCMCglmm(max.temp~Air.temp+
-                            as.factor(Year),
+                            as.factor(Year)+
+                            Rearing_brood_size,
                           random=~BirdID+ 
                             Brood.rearing+
                             Brood.natal,
@@ -107,15 +151,19 @@ modM1_Tbher_5 <- MCMCglmm(max.temp~Air.temp+
 summary(modM1_Tbher_5)
 plot(modM1_Tbher_5)
 
-posther.5<-modM1_Tbher_5$VCV[,"units"]/
+posther.5<-modM1_Tbher_5$VCV[,"BirdID"]/
   (modM1_Tbher_5$VCV[,"BirdID"]+modM1_Tbher_5$VCV[,"Brood.natal"]+modM1_Tbher_5$VCV[,"Brood.rearing"]+modM1_Tbher_5$VCV[,"units"])
 HPDinterval(posther.5,0.95)
 posterior.mode(posther.5)
 
-
+unique_to_vector2 <- setdiff(Age10.tb$BirdID, Tb.ped.new$BirdID)
+values_to_remove<-c("9989" , "9993" , "9998"  ,"10011" ,"10155" ,"10313", "10314" ,"10315" ,"10806" ,"10857" ,"10869", "10887", "10894")
+Age10.tb <- subset(Age10.tb, !BirdID %in% values_to_remove)
+Age10.tb<-as.data.frame(Age10.tb)
 
 modM1_Tbher_10 <- MCMCglmm(max.temp~Air.temp+
-                             as.factor(Year),
+                             as.factor(Year)+
+                             Rearing_brood_size,
                           random=~BirdID+ 
                             Brood.natal+
                             Brood.rearing,
@@ -124,13 +172,21 @@ modM1_Tbher_10 <- MCMCglmm(max.temp~Air.temp+
                           ginverse=list(BirdID=TB.Ainv), prior=prior_Tbher, verbose=FALSE)
 summary(modM1_Tbher_10)
 
-posther.10<-modM1_Tbher_10$VCV[,"Brood.rearing"]/
+posther.10<-modM1_Tbher_10$VCV[,"units"]/
   (modM1_Tbher_10$VCV[,"BirdID"]+modM1_Tbher_10$VCV[,"Brood.natal"]+modM1_Tbher_10$VCV[,"Brood.rearing"]+modM1_Tbher_10$VCV[,"units"])
 HPDinterval(posther.10,0.95)
 posterior.mode(posther.10)
 
+
+
+unique_to_vector2 <- setdiff(Age12.tb$BirdID, Tb.ped.new$BirdID)
+values_to_remove<-c("10011", "10155", "10313" ,"10314", "10315", "10806", "10857", "10869", "10887", "10894")
+Age12.tb <- subset(Age12.tb, !BirdID %in% values_to_remove)
+Age12.tb<-as.data.frame(Age12.tb)
+
 modM1_Tbher_12 <- MCMCglmm(max.temp~Air.temp+
-                             as.factor(Year),
+                             as.factor(Year)+
+                             Rearing_brood_size,
                           random=~BirdID+
                             Brood.natal+
                             Brood.rearing,
@@ -139,7 +195,7 @@ modM1_Tbher_12 <- MCMCglmm(max.temp~Air.temp+
                           ginverse=list(BirdID=TB.Ainv), prior=prior_Tbher, verbose=FALSE)
 summary(modM1_Tbher_12)
 
-posther.12<-modM1_Tbher_12$VCV[,"Brood.rearing"]/
+posther.12<-modM1_Tbher_12$VCV[,"units"]/
   (modM1_Tbher_12$VCV[,"BirdID"]+modM1_Tbher_12$VCV[,"Brood.natal"]+modM1_Tbher_12$VCV[,"Brood.rearing"]+modM1_Tbher_12$VCV[,"units"])
 HPDinterval(posther.12,0.95)
 posterior.mode(posther.12)
@@ -147,6 +203,7 @@ posterior.mode(posther.12)
 #---- temp by age plot ----
 library(ggplot2)
 variance.tempage<-read.csv("variance.tempbyage.csv")
+variance.tempage.broodsize<-read.csv("variance.tempbyage.broodsize.csv")
 str(variance.tempage)
 plot_tempage<-ggplot(data = variance.tempage,aes(x=as.factor(Age),y=Proportion.of.Variance,group=Trait,fill=Trait))+
   geom_line(size=0.8,aes(linetype=Trait))+
@@ -158,7 +215,8 @@ plot_tempage<-ggplot(data = variance.tempage,aes(x=as.factor(Age),y=Proportion.o
   theme(text=element_text(size=20),
         legend.title=element_blank())+
   xlab("Age")+
-  ylab("Proportion of Variance")
+  ylab("Proportion of Variance")+
+  ggtitle("Proportion of variance")
 plot_tempage
 
 #--------
