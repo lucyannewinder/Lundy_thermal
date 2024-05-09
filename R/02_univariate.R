c233403b11381a4e513caacbb57f75c1755b3915
lucyannewinder <47522409+lucyannewinder@users.noreply.github.com>
2024-05-09 10:52:30 +0100

    02_univariate.R

--- R/02_univariate.R
+++ R/02_univariate.R
@@ -27,19 +27,12 @@ Tb.dat.na <- read.csv("Data/Processed/data_for_analysis.csv")
 Age2.tb<-subset(Tb.dat.na,Age==2)
 Age2.tb<-droplevels(Age2.tb)
 
-# add brood size
-# does every chick have a record? is the number of phenotyped chicks a good measure of brood size?
-# L - yes except the 16 individuals that were removed so I will change this to add in those individuals.
-Age2.tb <- Age2.tb %>%
-  group_by(Brood.natal) %>%
-  mutate(Natal_brood_size = n())
-
-## what is this exclusion based on? There is no mention of this in the text
+## remove bird not in pedigree
 Age2.tb <- as.data.frame(subset(Age2.tb, BirdID != "10179"))
 
 # mean center covariates used in models 
 Age2.tb$Air.tempC <- scale(Age2.tb$Air.temp, scale=FALSE)
-Age2.tb$Natal_brood_sizeC <- scale(Age2.tb$Natal_brood_size, scale=FALSE)
+Age2.tb$Currentage_brood_sizeC <- scale(Age2.tb$Currentage_brood_size, scale=FALSE)
 
 
 ## get inverse A matrix for day 2 chicks
@@ -58,7 +51,7 @@ a=15
 
 mod_uni_day2 <- MCMCglmm(max.temp~Air.tempC+
                             as.factor(Year)+
-                            Natal_brood_sizeC,
+                           Currentage_brood_sizeC,
                      random=~BirdID+
                        Brood.natal,
                      data=Age2.tb,
@@ -76,19 +69,15 @@ summary(mod_uni_day2)
 Age5.tb<-subset(Tb.dat.na,Age==5)
 Age5.tb<-droplevels(Age5.tb)
 
-# add brood size
-Age5.tb <- Age5.tb %>%
-  group_by(Brood.rearing) %>%
-  mutate(Rearing_brood_size = n())
 
-### ?????
+# Remove birds not in pedigree
 values_to_remove<-c("10179" ,"10821", "10827", "10829" ,"10911")
 Age5.tb <- subset(Age5.tb, !BirdID %in% values_to_remove)
 Age5.tb<-as.data.frame(Age5.tb)
 
 # mean center covariates used in models 
 Age5.tb$Air.tempC <- scale(Age5.tb$Air.temp, scale=FALSE)
-Age5.tb$Rearing_brood_sizeC <- scale(Age5.tb$Rearing_brood_size, scale=FALSE)
+Age5.tb$Currentage_brood_sizeC <- scale(Age5.tb$Currentage_brood_size, scale=FALSE)
 
 ## get inverse A matrix for day 5 chicks
 Tb.ped.day5<- prunePed(Tb.ped,unique(Age5.tb$BirdID), make.base = TRUE)
@@ -106,7 +95,7 @@ prior_day5plus<-list(
 
 mod_uni_day5 <- MCMCglmm(max.temp~Air.tempC+
                             as.factor(Year)+
-                            Rearing_brood_sizeC,
+                           Currentage_brood_sizeC,
                           random=~BirdID+ 
                             Brood.rearing+
                             Brood.natal,
@@ -125,19 +114,14 @@ summary(mod_uni_day5)
 Age10.tb<-subset(Tb.dat.na,Age==10)
 Age10.tb<-droplevels(Age10.tb)
 
-# add brood size
-Age10.tb <- Age10.tb %>%
-  group_by(Brood.rearing) %>%
-  mutate(Rearing_brood_size = n())
-
-### ?????
+# remove values not in pedigree
 values_to_remove<-c("9989" , "9993" , "9998"  ,"10011" ,"10155" ,"10313", "10314" ,"10315" ,"10806" ,"10857" ,"10869", "10887", "10894")
 Age10.tb <- subset(Age10.tb, !BirdID %in% values_to_remove)
 Age10.tb<-as.data.frame(Age10.tb)
 
 # mean center covariates used in models 
 Age10.tb$Air.tempC <- scale(Age10.tb$Air.temp, scale=FALSE)
-Age10.tb$Rearing_brood_sizeC <- scale(Age10.tb$Rearing_brood_size, scale=FALSE)
+Age10.tb$Currentage_brood_sizeC <- scale(Age10.tb$Currentage_brood_size, scale=FALSE)
 
 ## get inverse A matrix for day 10 chicks
 Tb.ped.day10<- prunePed(Tb.ped,unique(Age10.tb$BirdID), make.base = TRUE)
@@ -146,7 +130,7 @@ day10.Ainv<-inverseA(Tb.ped.day10)$Ainv
 
 mod_uni_day10 <- MCMCglmm(max.temp~Air.tempC+
                             as.factor(Year)+
-                            Rearing_brood_sizeC,
+                            Currentage_brood_sizeC,
                           random=~BirdID+ 
                             Brood.rearing+
                             Brood.natal,
@@ -165,19 +149,15 @@ summary(mod_uni_day10)
 Age12.tb<-subset(Tb.dat.na,Age==12)
 Age12.tb<-droplevels(Age12.tb)
 
-# add brood size
-Age12.tb <- Age12.tb %>%
-  group_by(Brood.rearing) %>%
-  mutate(Rearing_brood_size = n())
 
-### ?????
+# remove birds not in pedigree
 values_to_remove<-c("10011", "10155", "10313" ,"10314", "10315", "10806", "10857", "10869", "10887", "10894")
 Age12.tb <- subset(Age12.tb, !BirdID %in% values_to_remove)
 Age12.tb<-as.data.frame(Age12.tb)
 
 # mean center covariates used in models 
 Age12.tb$Air.tempC <- scale(Age12.tb$Air.temp, scale=FALSE)
-Age12.tb$Rearing_brood_sizeC <- scale(Age12.tb$Rearing_brood_size, scale=FALSE)
+Age12.tb$Currentage_brood_sizeC <- scale(Age12.tb$Currentage_brood_size, scale=FALSE)
 
 ## get inverse A matrix for day 10 chicks
 Tb.ped.day12<- prunePed(Tb.ped,unique(Age12.tb$BirdID), make.base = TRUE)
@@ -186,7 +166,7 @@ day12.Ainv<-inverseA(Tb.ped.day12)$Ainv
 
 mod_uni_day12 <- MCMCglmm(max.temp~Air.tempC+
                             as.factor(Year)+
-                            Rearing_brood_sizeC,
+                            Currentage_brood_sizeC,
                           random=~BirdID+ 
                             Brood.rearing+
                             Brood.natal,
